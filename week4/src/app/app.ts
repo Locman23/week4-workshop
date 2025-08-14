d527add4b874d76a9f1d2ee37f7abffe43e5c706
Locman23 <Lachlans283@gmail.com>
2025-08-14 17:04:01 +1000

    Task 3

--- week4/src/app/app.ts
+++ week4/src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('week4');
+export class AppComponent {
+  title = signal('Week 4');
 }
